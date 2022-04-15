762cb69f22a4e00d38d98655a8e513fed7469a14
meshkaa <ali.meshkat@rabobank.nl>
2022-04-15 13:32:05 +0200

    generated project

---
+++ src/main/java/nl/rabobank/kotlinmovement/pizzarecipes/PizzaRecipesApplication.java
@@ -0,0 +1,13 @@
+package nl.rabobank.kotlinmovement.pizzarecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaRecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaRecipesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/rabobank/kotlinmovement/pizzarecipes/PizzaRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.rabobank.kotlinmovement.pizzarecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaRecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
