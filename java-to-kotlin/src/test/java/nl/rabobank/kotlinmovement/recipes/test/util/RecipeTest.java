9a8afaa4fbcb292359f8246456d6e1319b20eefe
meshkaa <ali.meshkat@rabobank.nl>
2022-10-28 10:59:16 +0200

    Use flyway for schema generation not hibernate. To make this work in the integration test the H2 in mem should close after the last connection (NOT DB_CLOSE_DELAY=-1). Unrelated small improvement: - moved out the plugins from the parent pom to its children - added a http file with example calls

--- java-to-kotlin/src/test/java/nl/rabobank/kotlinmovement/recipes/test/util/RecipeTest.java
+++ java-to-kotlin/src/test/java/nl/rabobank/kotlinmovement/recipes/test/util/RecipeTest.java
@@ -8,10 +8,12 @@ import nl.rabobank.kotlinmovement.recipes.test.util.model.RecipesErrorResponseTe
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -101,3 +103,4 @@ public class RecipeTest {
     }
 
 }
+
